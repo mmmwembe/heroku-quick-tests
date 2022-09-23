8ce7e699e88e1355d380378a76cbe7c4083793c1
Michael M <mmm111@hotmail.com>
2022-09-23 06:59:11 -0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# dust
+Update Page without Refreshing
+
+1) Creating a Virtual environment in Windows
+
+(a) Create a project directory (e.g file-uploader)
+(b) Change into the project directory
+cd C:\Users\O wner\Desktop\Heroku Projects\heroku-quick-tests
+
+2) Create virtual environ Run 
+    python3 -m venv <name_of_virtualenv>
+    python -m venv ./venv_quick_tests
+    This creates a virtual environment (a subdirectory) inside the 
+
+3) See structure of venv
+    tree venv_quick_tests
+4) Activate Virtual Environment
+
+      .\venv_quick_tests\Scripts\activate
+
+5) Installing Python Packages in Virtual Environment
+ 
+ pip install google-auth google-api-python-client google-cloud-storage
+ pip install python-dotenv pymongo dnspython
+ 
+ pip install flask pillow selenium pymongo passlib dnspython colormath colorthief pandas
+ pip install Flask-Dropzone // https://pypi.org/project/Flask-Dropzone/
+ pip install pillow  (so we can use the PIL library)
+ pip install flask pymongo passlib dnspython colormath colorthief pandas
+ 
+
+6) create a requirements.txt
+pip freeze > requirements.txt
+
+7) Run the app
+ 
+python app.py
+
+
+Competition of Soil Color Detector
+http://ncss-tech.github.io/AQP/aqp/color-contrast.html#25_Color_Chips_Arranged_by_Perceptual_Differences
+
+https://www.nrcs.usda.gov/wps/portal/nrcs/detail/soils/ref/?cid=nrcs142p2_053569
+
+https://observablehq.com/@philippkoytek/d3-advanced-drawing-custom-shapes
--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, session, redirect, url_for, request, j
 from flask_bootstrap import Bootstrap
 from functools import wraps
 from flask_pymongo import PyMongo
+from google.oauth2 import service_account
+from googleapiclient import discovery
+from google.cloud import storage as dns
+import os
 
 
 app = Flask(__name__)
@@ -11,10 +15,33 @@ app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 # Bootstrap
 bootstrap = Bootstrap(app)
 
+gcp_type = os.environ["TYPE"]
+gcp_project_id = os.environ["PROJECT_ID"] 
+gcp_private_key_id = os.environ["PRIVATE_KEY_ID"]
+gcp_private_key = os.environ["PRIVATE_KEY"]
+gcp_client_email = os.environ["CLIENT_EMAIL"]
+gcp_client_id =os.environ["CLIENT_ID"]
+gcp_auth_uri = os.environ["AUTH_URI"]
+gcp_token_uri = os.environ["TOKEN_URI"]
+gcp_auth_provider_x509_cert_url = os.environ["AUTH_PROVIDER_X509_CERT_URL"]
+gcp_client_x509_cert_url =os.environ["CLIENT_X509_CERT_URL"]
+
+gcp_sa_credentials={
+  "type": os.environ["TYPE"],
+  "project_id": os.environ["PROJECT_ID"],
+  "private_key_id": os.environ["PRIVATE_KEY_ID"],
+  "private_key": os.environ["PRIVATE_KEY"],
+  "client_email": os.environ["CLIENT_EMAIL"],
+  "client_id": os.environ["CLIENT_ID"],
+  "auth_uri": os.environ["AUTH_URI"],
+  "token_uri": os.environ["TOKEN_URI"],
+  "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
+  "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"]
+}
 
 @app.route('/')
 def home():
-  return render_template('classify-images.html')
+  return render_template('classify-images.html', data=gcp_sa_credentials)
 
 
 if __name__ == '__main__':
