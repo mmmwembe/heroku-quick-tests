e84ac11f72cdbff1b73898409d28235367489d8d
Michael M <mmm111@hotmail.com>
2022-09-24 12:04:07 -0500

    changes

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from flask_session import Session
 cluster =''
 app = Flask(__name__)
 # app.secret_key = b'\xcc^\x91\xea\x17-^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
-# app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = True
 # app.config["SESSION_TYPE"] = "filesystem"
 app.config['SECRET_KEY'] = os.urandom(24)
 # this is important or wont work
