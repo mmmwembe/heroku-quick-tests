147e5f35eb67d3d83121131fa19d82b1a8b4efc4
Michael M <mmm111@hotmail.com>
2022-09-23 21:47:16 -0500

    changes

--- app.py
+++ app.py
@@ -31,20 +31,34 @@ except:
 # 4) Uploaded credentials json to Heroku using buildpack and guidance from https://devdojo.com/bryanborge/adding-google-cloud-credentials-to-heroku
 
 
-def get_public_url_files_array_from_google_cloud_storage(bucket_name, sub_directory_path, target_file_types_array):
+def get_public_url_files_array_from_google_cloud_storage(bucket_name, bucket_sub_directory_path, target_file_types_array):
   # Example usage:
   #   bucket_name = '2021_tflite_glitch_models'
   #   sub_directory_path ='stack-plume-dust-classification/'
   #   target_file_types_array = ["tflite", "h5", "keras"]...another example: target_file_types_array = ["JPG", "JPEG", "jpg", "jpeg", "png", "PNG"]
   returned_public_urls =[]
   client = storage.Client()
-  for blob in client.list_blobs(bucket_name, prefix=sub_directory_path):
+  for blob in client.list_blobs(bucket_name, prefix=bucket_sub_directory_path):
     public_url = blob.public_url
     if public_url.endswith(tuple(target_file_types_array)):
       returned_public_urls.append(public_url)
 
   return returned_public_urls
 
+
+def upload_file_to_bucket(bucket_name, bucket_sub_dir_path, filename_of_file_being_uploaded, path_of_file_to_upload):
+    """ Upload data to a bucket"""
+    # The blob in the bucket is given the filename of the file being uploaded. 
+    returned_public_urls =[]
+    client = storage.Client()
+
+    bucket = client.get_bucket(bucket_name)
+    blob_full_path = os.path.join(bucket_sub_dir_path, filename_of_file_being_uploaded)
+    blob = bucket.blob(filename_of_file_being_uploaded)
+    blob.upload_from_filename(path_of_file_to_upload)
+    
+    #returns a public url
+    return blob.public_url
 
 
 
@@ -57,10 +71,13 @@ def home():
 
   model_urls = get_public_url_files_array_from_google_cloud_storage(bucket_name, sub_directory_path, target_file_types_array)
 
+  IMAGES_DIR='static/project-test-images/dust'
+  list_of_images = os.listdir(IMAGES_DIR)
+
   # model_urls =get_public_url_files_array_from_google_cloud_storage('2021_tflite_glitch_models', 'stack-plume-dust-classification/', ["tflite", "h5", "keras"])
   
 
-  return render_template('classify-images.html',models = model_urls, db = cluster["amina_db"])
+  return render_template('classify-images.html',models = model_urls, db = cluster["amina_db"], image_list = list_of_images)
 
 
 if __name__ == '__main__':
