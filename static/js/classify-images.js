d0954b90ad9b549b94a357ae0ebb6c596860e0e4
Michael M <mmm111@hotmail.com>
2022-09-25 02:03:01 -0500

    changes

--- static/js/classify-images.js
+++ static/js/classify-images.js
@@ -77,8 +77,8 @@ for(let i = 0; i < img_thumbnails.length; i++) {
     // Regenerate datatable so it maintains its font color
     // datatable =  $('#results-datatable').DataTable( {data: results_JSON, columns: [{ title: "#" },{ title: "Class/Label" },{ title: "Confidence" }],
     // searching: false,ordering: false,lengthChange: false} );
-    var firstColumnHeader = $('#results-datatable thead th');
-    firstColumnHeader.css('background', '#252526');
+    // var firstColumnHeader = $('#results-datatable thead th');
+    // firstColumnHeader.css('background', '#252526');
 
    })
 }
