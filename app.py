fc27b53997863b1bffb06e6538f54feec79a67b8
Michael M <mmm111@hotmail.com>
2022-09-28 17:49:43 -0500

    changes

--- app.py
+++ app.py
@@ -367,11 +367,11 @@ def upload_model():
 def upload_model2():
   
 	data = request.files
-	if 'models34567[]' not in request.files:
-		bucket_name = user_info["gcp_bucket_dict"]["bucket_name"]
-		sub_directory_path = user_info["gcp_bucket_dict"]["user_models_detection_subdir"] # user_models_detection_subdir user_images_subdir
-		client = storage.Client()
-		bucket_tmp = client.get_bucket(os.path.join(bucket_name, sub_directory_path,CURRENTLY_ACTIVE_FOLDER))
+	if 'models[]' not in request.files:
+		#bucket_name = user_info["gcp_bucket_dict"]["bucket_name"]
+		#sub_directory_path = user_info["gcp_bucket_dict"]["user_models_detection_subdir"] # user_models_detection_subdir user_images_subdir
+		#client = storage.Client()
+		#bucket_tmp = client.get_bucket(os.path.join(bucket_name, sub_directory_path,CURRENTLY_ACTIVE_FOLDER))
 		return redirect(request.url)
 
 	files = request.files.getlist('models[]')
