a05af4fbd64a21b191be8942bf600c7825d3c7f0
Michael M <mmm111@hotmail.com>
2022-09-27 19:46:17 -0500

    changes

--- app.py
+++ app.py
@@ -279,12 +279,10 @@ def upload_image():
 	elif request.form.get('which-form') == 'images-for-testing-classification': 
 		sub_directory_path = user_info["gcp_bucket_dict"]["user_test_images_subdir"]
 		target_file_types_array = ["JPG", "JPEG", "jpg", "jpeg", "png", "PNG"]
-  
-		# gcp_public_urls = upload_files_to_gcp(bucket_name, sub_directory_path, CURRENTLY_ACTIVE_FOLDER, files, target_file_types_array)
+		gcp_public_urls = upload_files_to_gcp(bucket_name, sub_directory_path, CURRENTLY_ACTIVE_FOLDER, files, target_file_types_array)
 		sub_dir_path_with_active_folder = os.path.join(sub_directory_path,CURRENTLY_ACTIVE_FOLDER)
 		gcp_active_directory_file_urls = get_public_url_files_array_from_google_cloud_storage(bucket_name, sub_dir_path_with_active_folder, target_file_types_array)
-  
-		return render_template('classify-images.html', filenames=[], images_in_dir=gcp_active_directory_file_urls)  
+		return render_template('classify-images.html', filenames=[], images_in_dir = gcp_active_directory_file_urls)  
    
 	elif request.form.get('which-form') == 'models-object-detection': 
 		sub_directory_path = user_info["gcp_bucket_dict"]["user_models_detection_subdir"]
