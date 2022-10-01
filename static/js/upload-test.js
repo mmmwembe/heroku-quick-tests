bce2d75bf5bd392aad3d20899c5dc96a4433d51f
Michael M <mmm111@hotmail.com>
2022-10-01 00:07:18 -0500

    changes

--- static/js/upload-test.js
+++ static/js/upload-test.js
@@ -1,44 +1,5 @@
 async function start() {
 
-
-   /*
-    const modelButtons = document.getElementsByClassName('modelbtn');
-
-    for(let i = 0; i < modelButtons.length; i++) {
-     modelButtons[i].addEventListener("click", function(e) {
-
-        var model_url = e.target.dataset.model
-        var model_name = e.target.dataset.modelname
-        var model_type = e.target.dataset.model_type
-        var task =  e.target.dataset.task
-
-        const body ={ "model_url": model_url, "model_name": model_name, "model_type": model_type, "task": task }
-
-        // var detection_model_url = btn_clicked.getAttribute('data-model')
-        // storeSessionValue("deployed_detection_model_url", detection_model_url)
-
-        alert('deploy model_url  ' + ' ' + model_url);
-        alert('model name : ' + ' ' + model_name + ' model_type : ' + model_type  + ' task : ' + task );
-
-
-
-        if (task =="delete"){
-
-            // deleteModel(model_url,model_name, model_type,task)
-
-            storeSessionValue("classification_model_url", model_url)
-
-
-        }
-
-
-
-        })
-
-    }
-
-  */
-
     task_btns = document.getElementsByClassName('actions_column');
 
             for(let i = 0; i < task_btns.length; i++) {
@@ -77,8 +38,6 @@ async function start() {
                 storeSessionValue("detection_model_url", model_url)
             }
 
-
-
             if (task =="view"){
 
                 // deleteModel(model_url,model_name, model_type,task)
@@ -96,7 +55,7 @@ async function start() {
 
     $.ajax({
         type: "POST",
-        url: "/NewDeleteModel/",
+        url: "/NewDeleteModel",
         dataType: 'json',
         data: { 
             model_url: model_url,
