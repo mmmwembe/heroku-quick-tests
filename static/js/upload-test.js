41dcc7b20402a211e7816ed3a0c6868aab89c11c
Michael M <mmm111@hotmail.com>
2022-10-01 13:40:24 -0500

    changes

--- static/js/upload-test.js
+++ static/js/upload-test.js
@@ -29,6 +29,38 @@ window.addEventListener('load', (event) => {
 
 
 
+    // Deploy Model
+    
+    $('.xDeployModel').on('click', function(e){
+
+
+        var model_id = $(this).attr('model_id')
+        var model_url = $('#model_url' +model_id ).val()
+        var model_name = $('#model_name' +model_id ).val()
+        var model_type = $('#model_type' +model_id ).val()
+        var task = $('task' +model_id ).val()
+
+        alert('model_id : ' + model_id + '  model_name : ' + model_name + ' model_type :' + model_type)
+
+        /*
+        req = $.ajax({
+                url: '/NewDeleteModel/',
+                type: 'POST',
+                data: { model_url : model_url, model_name : model_name, model_type : model_type, task : task }
+            });
+
+        req.done(function(data)
+        {
+        alert('Data from Server :' + data.everything)
+        });
+
+        */
+    }); 
+
+    
+
+
+
 
 
 
