6dbc15bea355cc87f70312424cdf8e84211a6062
Michael M <mmm111@hotmail.com>
2022-09-30 20:12:49 -0500

    changes

--- static/js/upload-test.js
+++ static/js/upload-test.js
@@ -1,104 +1,38 @@
 async function start() {
-    const img = document.querySelector("#selected-image");
-    var input = document.getElementById("image-selector");
-    const resultDiv = document.querySelector(".result");
-    const div_active_folder = document.getElementById("upload-folder");
-    const form_hidden_field_current_folder = document.getElementById("current_folder");
 
 
 
+    const modelButtons = document.getElementsByClassName('modelbtn');
 
-    var img_thumbnails = null
-    var active_folder = document.getElementById("upload-folder").value;
-    // alert('active folder ' + active_folder)
+    for(let i = 0; i < modelButtons.length; i++) {
+     modelButtons[i].addEventListener("click", function(e) {
 
-    let results_JSON =[];
-    var datatable = datatable =  $('#results-datatable').DataTable( {data: results_JSON, columns: [{ title: "#" },{ title: "Class/Label" },{ title: "Confidence" }],
-        searching: false,ordering: false,lengthChange: false} );
+        var model_url = e.target.dataset.model
+        var model_name = e.target.dataset.modelname
+        var model_type = e.target.dataset.model_type
+        var task =  e.target.dataset.task
 
-    var firstColumnHeader = $('#results-datatable thead th');
-    firstColumnHeader.css('background', '#252526');
+        const body ={ "model_url": model_url, "model_name": model_name, "model_type": model_type, "task": task }
 
-    //$('#results-datatable').css('color', 'black');
+        // var detection_model_url = btn_clicked.getAttribute('data-model')
+        // storeSessionValue("deployed_detection_model_url", detection_model_url)
 
-    // Load the TFLite model - Load the model from a custom url with other options (optional).
-    const model = await tfTask.ImageClassification.CustomModel.TFLite.load({
-        model: "https://storage.googleapis.com/2021_tflite_glitch_models/stack-plume-dust-classification/model_classifier.tflite",
-    });
+        alert('deploy model_url  ' + ' ' + model_url);
+        alert('model name : ' + ' ' + model_name + ' model_type : ' + model_type  + ' task : ' + task );
 
-    input.addEventListener("change", preview_image);
+        })
 
-    function preview_image(event) {
-        var reader = new FileReader();
-        reader.onload = function () {
-            img.src = reader.result;
-        };
-        reader.readAsDataURL(event.target.files[0]);
     }
 
-    document.querySelector("#predict-button").addEventListener("click", async () => {
-
-        // Run inference on an image.
-        // const predictions = await model.predict(img);
-        const predictions = await model.predict(document.querySelector("#selected-image"));
-        // console.log(predictions.classes);
-
-        // Show the results.
-        // resultDiv.textContent = predictions.classes.map((c) => `${c.className}: ${c.score.toFixed(3)}`).join(", ");
-
-        results_JSON = create_json_from_predictions(predictions)
-
-        datatable.clear();
-        datatable.rows.add(results_JSON);
-        datatable.draw();
-    });
-
-}
-
-function create_json_from_predictions(preds){
-    var jsonArr = [];
-    var json_object
-    
-    for (var i = 0; i < preds.classes.length; i++) {     
-        json_object = [i+1,preds.classes[i].className,((preds.classes[i].score*100).toFixed(0)).toString() + "%"]; 
-        jsonArr.push(json_object);
-       }
-      return jsonArr
-    }
-
-            
-img_thumbnails = document.getElementsByClassName('gallery_column');
-for(let i = 0; i < img_thumbnails.length; i++) {
-   img_thumbnails[i].addEventListener("click", function(e) {
-   var img_url = e.target.src
-   document.getElementById('selected-image').src = img_url;
-
-   // alert("image index = " + e.target.getAttribute('data-index'));
-
-    // Regenerate datatable so it maintains its font color
-    // datatable =  $('#results-datatable').DataTable( {data: results_JSON, columns: [{ title: "#" },{ title: "Class/Label" },{ title: "Confidence" }],
-    // searching: false,ordering: false,lengthChange: false} );
-    // var firstColumnHeader = $('#results-datatable thead th');
-    // firstColumnHeader.css('background', '#252526');
-
-
-    // Wait for 2 seconds (2000 milisecond) before re-analysing the image
-    /*
-    setTimeout(function (){
-  
-        const predictions = await model.predict(document.querySelector("#selected-image"));
-        results_JSON = create_json_from_predictions(predictions)
-        datatable.clear();
-        datatable.rows.add(results_JSON);
-        datatable.draw();
-                  
-      }, 2000);
-
-      */
 
 
 
-   })
+
+
+
+
+
+
 }
     
 
