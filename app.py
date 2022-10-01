5b2062dc15311d603a385bddedcbc5cd40c9928b
Michael M <mmm111@hotmail.com>
2022-10-01 10:01:10 -0500

    changes

--- app.py
+++ app.py
@@ -715,7 +715,7 @@ def NewDeleteModel():
 		#model_name = request.form['model_name']
 		#model_type = request.form['model_type']
 		#task = request.form['task']
-		everything="POST METHOD : model_url " +  args # +  model_url 
+		everything="POST METHOD : model_url " +  str(args) # +  model_url 
 		##everything = "INFORMATION FROM SERVER - " + "model_url: " + model_url + "model_name : " + model_name + " model_type: " + model_type + " task :" + task
    
 	else:
