5b06d72110098193d97bb1d0a7e098bffa366f3c
Michael M <mmm111@hotmail.com>
2022-10-11 18:25:13 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -2,15 +2,43 @@ window.addEventListener('load', (event) => {
 
 
     img_thumbnails = document.getElementsByClassName('gallery_column');
-    
+
+    showFirstImage()
+
     for(let i = 0; i < img_thumbnails.length; i++) {
       img_thumbnails[i].addEventListener("click", function(e) {
 
                 img_url = e.target.src
 
-                alert(img_url)
+                //alert(img_url)
 
       })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function showFirstImage(){
+        var first_img_div = document.getElementsByClassName('gallery_column')[0]
+        var first_img = first_img_div.getAttribute('src');
+        alert(first_img)
     }
 
 
