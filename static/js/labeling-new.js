270e35f4dd2edcbbacbe8d08bbd9a331a95c61bd
Michael M <mmm111@hotmail.com>
2022-10-11 18:18:32 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -1,10 +1,17 @@
 window.addEventListener('load', (event) => {
 
 
+    img_thumbnails = document.getElementsByClassName('gallery_column');
+    
+    for(let i = 0; i < img_thumbnails.length; i++) {
+      img_thumbnails[i].addEventListener("click", function(e) {
 
-    $(document).ready(function () {
-        $('#GradingTable').DataTable();
-    });
+                img_url = e.target.src
+
+                alert(img_url)
+
+      })
+    }
 
 
 
