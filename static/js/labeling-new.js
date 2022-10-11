1265c82d78dd0d8b0e25fbb0bcda95260bf4d15c
Michael M <mmm111@hotmail.com>
2022-10-11 18:34:46 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -3,6 +3,8 @@ window.addEventListener('load', (event) => {
 
     img_thumbnails = document.getElementsByClassName('gallery_column');
 
+    // Show the first image from the thumbnails the main image
+
     showFirstImage()
 
     for(let i = 0; i < img_thumbnails.length; i++) {
@@ -37,8 +39,8 @@ window.addEventListener('load', (event) => {
 
     function showFirstImage(){
         var first_img_div = document.getElementsByClassName('gallery_column')[0]
-        //var first_img = first_img_div.src;
-        alert(first_img_div)
+        var first_img = first_img_div.getElementsByTagName('img')[0].src;
+        alert(first_img)
     }
 
 
