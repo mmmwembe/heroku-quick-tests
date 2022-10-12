8e7404553f8827c7f18c83cb374e9a2b33933cb8
Michael M <mmm111@hotmail.com>
2022-10-11 19:18:35 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -27,6 +27,11 @@ window.addEventListener('load', (event) => {
         remoteImageForFabric.crossOrigin = "Anonymous";
         remoteImageForFabric.src = "";
         remoteImageForFabric.onload = function(loadedImage) {
+
+            orgWidth = loadedImage.width;
+            orgHeight = loadedImage.height;
+            alert('Original Width : ' + orgWidth +" Original Height " + orgHeight)
+
             var imgToDrawOnFabricCanvas = new fabric.Image(remoteImageForFabric);
             fabricCanvas.add(imgToDrawOnFabricCanvas);
         }
@@ -34,6 +39,23 @@ window.addEventListener('load', (event) => {
         remoteImageForFabric.src = NEW_IMAGE_URL;
 
    }
+
+    /**
+  * Conserve aspect ratio of the original region. Useful when shrinking/enlarging
+  * images to fit into a certain area.
+  *
+  * @param {Number} srcWidth width of source image
+  * @param {Number} srcHeight height of source image
+  * @param {Number} maxWidth maximum available width
+  * @param {Number} maxHeight maximum available height
+  * @return {Object} { width, height }
+  */
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+    return { width: srcWidth*ratio, height: srcHeight*ratio };
+ }
 
 
 
