be5ee0f64ce4d5c916c00e7422be49666784d12e
Michael M <mmm111@hotmail.com>
2022-10-12 17:56:38 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -142,22 +142,22 @@ fabricCanvas.on('mouse:up', function(o){
 
 
 
-    
+    fabricCanvas.on("mousedblclick", ()=>{
+
+        activeObject = fabricCanvas.getActiveObject();
+
+        alert(' active Object ' + activeObject)
+
+    }) 
 
 
 
 
-    fabricCanvas.dblclick(function(e) { 
 
-      activeObject = fabricCanvas.getActiveObject();
-
-      alert(' active Object ' + activeObject)
-
-    }); 
     
     
     // Double click function for canvas
-    fabric = (function(f) { var nativeOn = f.on; var dblClickSubscribers = []; var nativeCanvas = f.Canvas;   f.Canvas = (function(domId, options) { var canvasDomElement = document.getElementById(domId); var c = new nativeCanvas(domId, options);   c.dblclick = function(handler) { dblClickSubscribers.push(handler) };   canvasDomElement.nextSibling.ondblclick = function(ev){ for(var i = 0; i < dblClickSubscribers.length; i++) { console.log(ev); dblClickSubscribers[i]({ e :ev }); } }; return c; });   return f; }(fabric)); 
+  
     
     
     
