0e078d2874a51087abdf6f85703ea4a6cbbd51ed
Michael M <mmm111@hotmail.com>
2022-10-14 20:17:29 -0500

    chnages

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -408,8 +408,8 @@ fabricCanvas.on('mouse:dblclick', (e1) => {
                 keys.forEach(k => {
                 //if(data3[k].leadershipSkills.commander){
                 //    result[k] = data3[k]
-                  if (canvas_json_object[k].type =="rect"){
-                      alert(JSON.stringify(canvas_json_object[k].type =="rect"))
+                  if (canvas_json_object[k] =="rect"){
+                      alert(JSON.stringify(canvas_json_object[k]))
                 //    alert(' result[k] ' + JSON.stringify(result[k]["leadershipSkills"]))
                    }
                 //}
