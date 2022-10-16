47f29de6de1ca7222a7c3272778d2ec85a5307ba
Michael M <mmm111@hotmail.com>
2022-10-16 12:08:36 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -60,8 +60,6 @@ window.addEventListener('load', (event) => {
 
     img_thumbnails = document.getElementsByClassName('gallery_column');
 
-
-
     var IMAGE_LIST = {}
     for(let i = 0; i < img_thumbnails.length; i++) {
       var imageURL = img_thumbnails[i].getElementsByTagName('img')[0].src;
