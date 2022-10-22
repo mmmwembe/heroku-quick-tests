b1be747b775fa1be76439b26a76d053eede440d2
Michael M <mmm111@hotmail.com>
2022-10-21 22:40:39 -0400

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -42,7 +42,7 @@ window.addEventListener('load', (event) => {
 
     var first_20_colors = ['#112FDF', '#FF0006', '#00A546','#D95C00', '#862E85', '#AFD800','#512479', '#31CBF1', '#FCAE03','#FC368D', '#723BB0', '#E12A1F','#FF014A', '#0094D4', '#879AF9','#E40061', '#F7DC43', '#3C55E6','#590F26', '#243274'];
 
-    var user_id ="10101010102030232"
+    var user_id ="45f20651684f40be8916c29d69295998"
 
 
     // https://www.demo2s.com/javascript/javascript-fabric-js-draw-rectangle-between-two-mouse-clicks-on-canvas.html
