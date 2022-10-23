79d8f4fb33cbd656e8905dd233f6911320721cf0
Michael M <mmm111@hotmail.com>
2022-10-22 21:01:41 -0400

    changes

--- app.py
+++ app.py
@@ -982,8 +982,8 @@ def get_all_projects():
 	for result in results:
 		all_projects.append(result) # 
 
-	#return jsonify(all_projects = all_projects)
-	return render_template('labeling-new.html', all_projects = all_projects)
+	return jsonify(all_projects = all_projects)
+	# return render_template('labeling-new.html', all_projects = all_projects)
 
 
 
