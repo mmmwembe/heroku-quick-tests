1c73610b1327840245c4441480fb63d36de899d0
Michael M <mmm111@hotmail.com>
2022-10-25 19:12:14 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -969,8 +969,8 @@ fabricCanvas.on('mouse:dblclick', (e1) => {
         $("#currentLabel").css("margin", "5px");
         $("#currentLabel").html("")          
         $("#currentLabel").html(_current_label)
-        alert(' line 972 - maybe this might be the culprit ')    
         $("#currentLabel").animate({height: '50px',width: '220px'},2000)
+        alert(' line 973 - perhaps this is it now...')    
         $("#currentLabel").verticalAlign();
 
         alert(' line 976 - showCurrentLabel is executed OK!!!')
