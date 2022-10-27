3ae1390684dd7cdc439121f03936bd2d61063fc5
Michael M <mmm111@hotmail.com>
2022-10-26 21:00:49 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -2209,9 +2209,8 @@ $("#delete_project_btn").click(function (){
 
 
 
-
-var lat_lng = {{ lat_lng|tojson|safe }};
-alert('line 2214 - ' + JSON.stringify(lat_lng))
+var parsed = JSON.parse('{{ lat_lng | tojson}}');
+alert('line 2214 - ' + JSON.stringify(parsed))
 
 
 
