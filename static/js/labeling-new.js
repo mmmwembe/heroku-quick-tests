1e33daef2e656d973710a5894b7f5d37c4972cc2
Michael M <mmm111@hotmail.com>
2022-11-01 17:02:15 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -2697,6 +2697,73 @@ function getJinJaVariablesFromHtmlFile() {
 
 
 
+function show_label_buckets_from_server_json_data(json_data){
+
+    var active_project_id = json_data.active_project
+    var active_label = json_data.active_label
+    var active_project_result = json_data.active_project_result
+
+    // Set Environment Variables
+    ACTIVE_PROJECT_ID = active_project_id
+    ACTIVE_PROJECT_JSON = active_project_result
+    CURRENT_PROJECT = ACTIVE_PROJECT_JSON['project_name']
+    LABELS_COLOR_MAP = ACTIVE_PROJECT_JSON['labels_color_map']
+    PROJECT_JSON = ACTIVE_PROJECT_JSON
+
+    window.localStorage.setItem("project_json",JSON.stringify(ACTIVE_PROJECT_JSON))
+    window.localStorage.setItem("labels_color_map",JSON.stringify(LABELS_COLOR_MAP))
+
+    data = [];
+
+    counter = 0
+
+    Object.keys(LABELS_COLOR_MAP).forEach(function(key) {
+        var label = key
+        var color = LABELS_COLOR_MAP[key]
+
+        var current_label_info = filter_project_json_by_label(key)
+
+        var label_id = current_label_info['label_id']
+
+        // Attributes for each label...can use this information in the future
+        var original_image_urls = current_label_info['original_image_urls']
+        var all_jpeg_image_urls = current_label_info['all_jpeg_image_urls']
+        var cropped_image_urls = current_label_info['cropped_image_urls']
+        var augmentation_image_urls = current_label_info['augmentation_image_urls']
+        var original_image_label_jsons = current_label_info['original_image_label_jsons']
+        var all_jpeg_image_label_jsons = current_label_info['all_jpeg_image_label_jsons']
+        var augmentation_image_label_jsons = current_label_info['augmentation_image_label_jsons']
+        var number_original_images = current_label_info['original_image_urls'].length
+        var number_all_jpeg_images = current_label_info['all_jpeg_image_urls'].length
+        var number_cropped_images = current_label_info['cropped_image_urls'].length
+        var number_augmentation_images = current_label_info['augmentation_image_urls'].length
+        var original_image_label_jsons = current_label_info['original_image_label_jsons']
+        var all_jpeg_image_label_jsons = current_label_info['all_jpeg_image_label_jsons']
+        var augmentation_image_label_jsons = current_label_info['augmentation_image_label_jsons']
+        var date_created = current_label_info['date_created']
+        var date_modified = current_label_info['date_modified']
+    
+        var data_element = {"index": counter, "label": label,"color": color, 
+                            "num_images": number_original_images, "labeled_images": "", 
+                            "all_labeled_true_false": "", "project_id": ACTIVE_PROJECT_JSON['project_js_id'],
+                            "project_name": ACTIVE_PROJECT_JSON['project_name'], "user_id": ACTIVE_PROJECT_JSON['user_id'], 
+                            "ISODate": date_created, "date_created": date_created, 
+                            "original_image_urls": original_image_urls,"all_jpeg_image_urls": all_jpeg_image_urls}
+
+        data.push(data_element)
+        
+        counter += 1;
+
+    })
+
+    // Update Page
+    updatePage()
+
+    //show_label_buckets()
+    new_create_label_buckets(data)
+
+}
+
 
 
 
