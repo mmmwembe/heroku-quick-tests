001564d903e23c21162fcf966e6539217a05c3aa
Michael M <mmm111@hotmail.com>
2022-11-05 09:06:18 -0400

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -973,7 +973,7 @@ fabricCanvas.on('mouse:dblclick', (e1) => {
         $("#p_tag_currentLabel").css('color', 'white');
         $("#p_tag_currentLabel").css("width", "90%");
         $("#p_tag_currentLabel").css("height", "50px");
-        $("#p_tag_currentLabel").css("padding", "5px");
+        $("#p_tag_currentLabel").css("padding", "0px");
         $("#p_tag_currentLabel").css("margin", "auto");        
         $("#p_tag_currentLabel").html("")   
         $("#p_tag_currentLabel").html(_current_label)   
