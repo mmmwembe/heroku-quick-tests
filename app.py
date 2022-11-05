4a1335ce4b31a45edb0c447652399562f5a89c1b
Michael M <mmm111@hotmail.com>
2022-11-05 13:06:13 -0400

    changes

--- app.py
+++ app.py
@@ -1198,26 +1198,16 @@ def get_active_project2():
         
         query ={'user_id': user_id}
         user_session_info = user_session_data.find(query)
-        
-        active_project_id =''
-        active_label =''  
-        results = [] 
-        active_project_result = {}
-        
-        try:
-            active_project_id = user_session_info[0]['active_project']
-            active_label = user_session_info[0]['active_label']         
-        except:
-            pass
-        
-        if active_project_id: 
-            try:
-                active_project_query = {'project_js_id': active_project_id,  'user_id': user_id}
-                results = user_projects.find(active_project_query)
+        active_project_id = user_session_info[0]['active_project']
+        active_label = user_session_info[0]['active_label']
+        active_project_query = {'project_js_id': active_project_id,  'user_id': user_id}
+        results = user_projects.find(active_project_query)
+        active_project_result = results[0]
+        # active_project_id =''
+        # active_label =''  
+        # results = [] 
+        # active_project_result = {}
 
-                active_project_result = results[0]  
-            except:
-                pass
             
     return jsonify(active_project_id = active_project_id, active_label = active_label, active_project_result = active_project_result)
 
