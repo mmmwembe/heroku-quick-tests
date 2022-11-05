5cc894560e21f3f9aba0abf1ee0fcdbf01f6e9e6
Michael M <mmm111@hotmail.com>
2022-11-05 10:44:48 -0400

    changes

--- static/js/lab_labelling.js
+++ static/js/lab_labelling.js
@@ -469,3 +469,117 @@ function show_label_buckets_from_server_json_data(json_data){
 
 }
 
+
+
+
+
+
+
+
+
+
+
+function update_label_bucket(data){
+
+var active_project_id = data.active_project_id
+var active_label = data.active_label
+var active_project_result = data.active_project_result[0]
+alert('/get_active_project')
+alert('line 1806 -- active_project : ' + active_project_id)
+//alert('active_label: ' + active_label)
+alert(' line 1822 active_project_result  ' + JSON.stringify(active_project_result))
+
+ACTIVE_PROJECT_ID = active_project_id
+ACTIVE_PROJECT_JSON = active_project_result
+CURRENT_PROJECT = ACTIVE_PROJECT_JSON['project_name']
+LABELS_COLOR_MAP = ACTIVE_PROJECT_JSON['labels_color_map']
+PROJECT_JSON = ACTIVE_PROJECT_JSON
+
+window.localStorage.setItem("project_json",JSON.stringify(ACTIVE_PROJECT_JSON))
+window.localStorage.setItem("labels_color_map",JSON.stringify(LABELS_COLOR_MAP))
+
+data = [];
+
+counter = 0
+
+Object.keys(LABELS_COLOR_MAP).forEach(function(key) {
+    var label = key
+    var color = LABELS_COLOR_MAP[key]
+
+    var current_label_info = filter_project_json_by_label(key)
+
+    var label_id = current_label_info['label_id']
+
+    // Attributes for each label...can use this information in the future
+    var original_image_urls = current_label_info['original_image_urls']
+    var all_jpeg_image_urls = current_label_info['all_jpeg_image_urls']
+    var cropped_image_urls = current_label_info['cropped_image_urls']
+    var augmentation_image_urls = current_label_info['augmentation_image_urls']
+    var original_image_label_jsons = current_label_info['original_image_label_jsons']
+    var all_jpeg_image_label_jsons = current_label_info['all_jpeg_image_label_jsons']
+    var augmentation_image_label_jsons = current_label_info['augmentation_image_label_jsons']
+    var number_original_images = current_label_info['original_image_urls'].length
+    var number_all_jpeg_images = current_label_info['all_jpeg_image_urls'].length
+    var number_cropped_images = current_label_info['cropped_image_urls'].length
+    var number_augmentation_images = current_label_info['augmentation_image_urls'].length
+    var original_image_label_jsons = current_label_info['original_image_label_jsons']
+    var all_jpeg_image_label_jsons = current_label_info['all_jpeg_image_label_jsons']
+    var augmentation_image_label_jsons = current_label_info['augmentation_image_label_jsons']
+    var date_created = current_label_info['date_created']
+    var date_modified = current_label_info['date_modified']
+   
+    var data_element = {"index": counter, "label": label,"color": color, 
+                        "num_images": number_original_images, "labeled_images": "", 
+                        "all_labeled_true_false": "", "project_id": ACTIVE_PROJECT_JSON['project_js_id'],
+                        "project_name": ACTIVE_PROJECT_JSON['project_name'], "user_id": ACTIVE_PROJECT_JSON['user_id'], 
+                        "ISODate": date_created, "date_created": date_created, 
+                        "original_image_urls": original_image_urls,"all_jpeg_image_urls": all_jpeg_image_urls}
+
+    data.push(data_element)
+    
+    counter += 1;
+
+})
+alert(' Line 1895 data obect ' + JSON.stringify(data))   
+
+// Update Page
+
+updatePage()
+
+alert(' Line 1901 data obect - confirmation that it gets past the updatePage() function ')   
+//alert(' Line 1887 - this is the first gate I see')   
+
+//show_label_buckets()
+new_create_label_buckets(data)
+
+// create_label_buckets(data)
+
+for (var i = 0; i < data.length; i += 4) {
+
+    var new_row = document.createElement('div')
+    new_row.className="row"
+
+    // alert('line 1897 - it gets here' )
+
+    var card_1_data = data.hasOwnProperty(i) ? data[i]  : " ";  
+    var card_2_data = data.hasOwnProperty(i+1) ? data[i+1] : " ";   
+    var card_3_data = data.hasOwnProperty(i+2) ? data[i+2]  : " ";   
+    var card_4_data = data.hasOwnProperty(i+3) ? data[i+3] : " ";   
+
+    // alert('line 1904 - it gets also here' )
+    
+    card_1_string = create_card(card_1_data)
+    card_2_string = create_card(card_2_data)
+    card_3_string = create_card(card_3_data)
+    card_4_string = create_card(card_4_data)
+
+    var combined_card_string = card_1_string + '\n' + card_2_string + '\n' + card_3_string + '\n' + card_4_string
+
+    new_row.innerHTML = combined_card_string
+
+    document.getElementById('label_buckets_container').appendChild(new_row)
+
+}
+
+
+}
