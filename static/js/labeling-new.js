d5458734c63e37c7fa33accf823f0437126e3230
Michael M <mmm111@hotmail.com>
2022-11-06 07:33:04 -0500

    changes

--- static/js/labeling-new.js
+++ static/js/labeling-new.js
@@ -2707,8 +2707,8 @@ function NewLabelBucketCard(data_element){
             
 
 
-            document.getElementById(p2_id).innerHTML ="20"
-            document.getElementById(p3_id).innerHTML ="0"
+            //document.getElementById(p2_id).innerHTML ="20"
+            //document.getElementById(p3_id).innerHTML ="0"
         
             //$('#' + p2_id).html('Number of Images: ' + number_original_images);  
             //$('#' + p3_id).html('Labelled Images: ' +  number_labelled_original_image_urls);  
