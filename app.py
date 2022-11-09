9710265af22b2a9f7a362f7b7b319ced81d95675
Michael M <mmm111@hotmail.com>
2022-11-08 20:28:00 -0600

    changes

--- app.py
+++ app.py
@@ -1441,9 +1441,10 @@ def add_label_records():
         
         # Update original_images_normalized_dataset for the label
         user_projects.update_one({ "labels.label": active_label_bucket, 'user_id': user_id,'project_js_id': project_id }, { "$set": { "labels.$.labelled_original_image_urls": labelled_images_array } })          
-        
+        time.sleep(1)        
     	# Update original_images_normalized_dataset for the label
         user_projects.update_one({ "labels.label": active_label_bucket, 'user_id': user_id,'project_js_id': project_id }, { "$set": { "labels.$.original_images_normalized_dataset": label_record_item } })
+        time.sleep(1)
         user_projects.update_one({ "labels.label": active_label_bucket, 'user_id': user_id,'project_js_id': project_id }, { "$set": { "labels.$.original_image_label_jsons": original_image_label_jsons_dict_from_json_string} })                  
 
           
