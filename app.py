19f1782b0b0536f6f9556060eead6c5b8a086d92
Michael M <mmm111@hotmail.com>
2022-12-04 09:02:57 -0600

    changes

--- app.py
+++ app.py
@@ -2065,13 +2065,10 @@ def delete_model_item():
  
  	# Delete the model_item for model_id retrieved
 	if model_type=="classification":
-		pass
 		user_projects.update_one({'user_id': session["user"]["_id"],'models.classification_models._id': model_id},{ '$pull': { 'models.classification_models': {'_id': model_id } } })
 	elif model_type=="object detection":
-		pass
 		user_projects.update_one({'user_id': session["user"]["_id"],'models.object_detection_models._id': model_id},{ '$pull': { 'models.object_detection_models': {'_id': model_id } } }) 
 	elif model_type=="audio classification":
-		pass
 		user_projects.update_one({'user_id': session["user"]["_id"],'models.audio_classification_models._id': model_id},{ '$pull': { 'models.audio_classification_models': {'_id': model_id } } })             
 	else  :
 		pass   
