6403872eb96e7f65229ef669b97376405439fc4c
Michael M <mmm111@hotmail.com>
2023-02-07 19:24:52 -0600

    changes

--- app.py
+++ app.py
@@ -1383,13 +1383,14 @@ def saveCroppedImage200():
         img = Image.open(BytesIO(base64.decodebytes(bytes(cropped_image_dataURL, "utf-8"))))
         # img.save(cropped_image_file_path)
         # saveImageBase42StringAsImage(cropped_image_dataURL)
-        img_bytes = img.resize((rect_width, rect_height), 2).tobytes()
+        # img_bytes = img.resize((rect_width, rect_height), 2).tobytes()
 
         # static/images/user1/canvas_jsons
         
         # encoded_string = base64.b64encode(cropped_image_dataURL)
+        # https://stackoverflow.com/questions/55941068/change-image-size-with-pil-in-a-google-cloud-storage-bucket-from-a-vm-in-gcloud
    
-    return jsonify(result = 'success', url=img_bytes)
+    return jsonify(result = 'success', url=rect_width)
 
 
 @app.route('/image_url/', methods=['POST','GET'])
