04a47e74bc02a55c2e95735589b640868043db10
Michael M <mmm111@hotmail.com>
2023-02-21 18:36:13 -0600

    changes

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,53 +1,51 @@
-// Initialize canvas
-const canvas = new fabric.Canvas('fabricCanvas');
+// Get the canvas element and set its width
+const canvas = document.getElementById("fabricCanvas");
+canvas.width = window.innerWidth * 0.5;
 
-// Display background image
-let imageUrl = 'https://storage.googleapis.com/amina-files/users/92520572d6ac46b98a61745b61c327e7/user-images/ldcd9eygkpgul4i57a/Cherry_tomatoes/White-American-Rabbit.jpg';
-const backgroundImage = new Image();
-backgroundImage.crossOrigin = 'anonymous';
-backgroundImage.onload = function () {
-  canvas.setBackgroundImage(backgroundImage.src, canvas.renderAll.bind(canvas), {
-    scaleX: canvas.width / backgroundImage.width,
-    scaleY: canvas.height / backgroundImage.height,
-  });
+// Get the canvas context
+const ctx = canvas.getContext("2d");
+
+// Set the initial background image
+let imageUrl = "https://storage.googleapis.com/amina-files/users/92520572d6ac46b98a61745b61c327e7/user-images/ldcd9eygkpgul4i57a/Cherry_tomatoes/White-American-Rabbit.jpg";
+const img = new Image();
+img.crossOrigin = "anonymous";
+img.onload = function() {
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.width * img.height / img.width);
 };
-backgroundImage.src = imageUrl;
+img.src = imageUrl;
 
-// Change background image on next button click
-const nextBtn = document.getElementById('nextBtn');
-nextBtn.addEventListener('click', () => {
-  imageUrl = 'https://storage.googleapis.com/amina-files/users/92520572d6ac46b98a61745b61c327e7/user-images/ldcd9eygkpgul4i57a/Grape_tomatoes/american-rabbit-001.jpg';
-  backgroundImage.src = imageUrl;
+// Get the button elements
+const nextBtn = document.getElementById("nextBtn");
+const cropBtn = document.getElementById("cropBtn");
+const autoCropBtn = document.getElementById("autoCropBtn");
+
+// Add event listeners to the buttons
+nextBtn.addEventListener("click", function() {
+  imageUrl = "https://storage.googleapis.com/amina-files/users/92520572d6ac46b98a61745b61c327e7/user-images/ldcd9eygkpgul4i57a/Grape_tomatoes/american-rabbit-001.jpg";
+  img.src = imageUrl;
 });
 
-// Crop and display section of canvas on crop button click
-const cropBtn = document.getElementById('cropBtn');
-cropBtn.addEventListener('click', () => {
-  const croppedImage = new Image();
-  croppedImage.src = canvas.toDataURL({
-    left: 0,
-    top: 0,
-    width: 150,
-    height: 50,
-  });
-  const croppedImageElement = document.createElement('img');
-  croppedImageElement.src = croppedImage.src;
+cropBtn.addEventListener("click", function() {
+  const croppedCanvas = document.createElement("canvas");
+  const croppedCtx = croppedCanvas.getContext("2d");
+  croppedCanvas.width = 150;
+  croppedCanvas.height = 50;
+  croppedCtx.drawImage(canvas, 0, 0, croppedCanvas.width, croppedCanvas.height, 0, 0, croppedCanvas.width, croppedCanvas.height);
+
+  const croppedImageElement = document.createElement("img");
+  croppedImageElement.src = croppedCanvas.toDataURL();
   document.body.appendChild(croppedImageElement);
 });
 
-// Append cropped image to div on auto crop button click
-const autoCropBtn = document.getElementById('autoCropBtn');
-autoCropBtn.addEventListener('click', () => {
-  // alert('autoCropBtn clicked');
-  const croppedImage = new Image();
-  croppedImage.src = canvas.toDataURL({
-    left: 0,
-    top: 0,
-    width: 150,
-    height: 50,
-  });
-  const croppedImageElement = document.createElement('img');
-  croppedImageElement.src = croppedImage.src;
-  const autoCropDiv = document.getElementById('autoCropDiv');
-  autoCropDiv.appendChild(croppedImageElement);
+autoCropBtn.addEventListener("click", function() {
+  // alert("autoCropBtn clicked");
+  const croppedCanvas = document.createElement("canvas");
+  const croppedCtx = croppedCanvas.getContext("2d");
+  croppedCanvas.width = 150;
+  croppedCanvas.height = 50;
+  croppedCtx.drawImage(canvas, 0, 0, croppedCanvas.width, croppedCanvas.height, 0, 0, croppedCanvas.width, croppedCanvas.height);
+
+  const croppedImageElement = document.createElement("img");
+  croppedImageElement.src = croppedCanvas.toDataURL();
+  document.getElementById("autoCropDiv").appendChild(croppedImageElement);
 });
