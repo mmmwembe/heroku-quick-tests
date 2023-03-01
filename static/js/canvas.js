3bea44eabbdae153f5ab79ed346937350802dead
Michael M <mmm111@hotmail.com>
2023-02-28 19:36:18 -0600

    changes

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,7 +1,16 @@
 window.onload = function() {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  canvas.width = window.innerWidth / 2;
-  canvas.height = window.innerWidth / 4;
-  canvas.style.margin = "0 auto";
-}
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+
+  // Set canvas dimensions to fill middle column of first row
+  canvas.width = document.querySelector('.col-6').offsetWidth;
+  canvas.height = document.querySelector('.col-6').offsetHeight;
+
+  // Add event listener to the crop button
+  var cropBtn = document.getElementById('cropBtn');
+  cropBtn.addEventListener('click', function() {
+    var croppedImg = new Image();
+    croppedImg.src = canvas.toDataURL();
+    document.getElementById('croppedImgDiv').appendChild(croppedImg);
+  });
+};
