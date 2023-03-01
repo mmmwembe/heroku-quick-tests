adcee59cb86cef5a1c85b5c27cf0d75801c45298
Michael M <mmm111@hotmail.com>
2023-02-28 18:56:28 -0600

    changes

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,24 +1,7 @@
-// Get the canvas element and set its dimensions
-var canvas = document.getElementById("canvas");
-canvas.width = canvas.parentNode.offsetWidth * 0.5;
-canvas.height = canvas.parentNode.offsetHeight * 0.25;
-
-// Add event listeners to the buttons
-var nextBtn = document.getElementById("nextBtn");
-var cropBtn = document.getElementById("cropBtn");
-var autoCropBtn = document.getElementById("autoCropBtn");
-
-nextBtn.addEventListener("click", function() {
-	// Handle the "Next" button click
-  alert("// Handle the Next button click")
-});
-
-cropBtn.addEventListener("click", function() {
-	// Handle the "Crop" button click
-  alert("// Handle the Crop button click")
-});
-
-autoCropBtn.addEventListener("click", function() {
-	// Handle the "Auto Crop" button click
-  alert("// Handle the Auto Crop button click")
-});
+window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  canvas.width = window.innerWidth / 2;
+  canvas.height = window.innerWidth / 4;
+  canvas.style.margin = "0 auto";
+}
